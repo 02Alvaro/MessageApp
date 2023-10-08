f0a47db96da9259438ab66992a12b00c48ea4411
Álvaro <90500200+02Alvaro@users.noreply.github.com>
2023-10-08 11:42:36 +0200

    feat: :rocket: application service

---
+++ messaging/src/Application/MessageService.cs
@@ -0,0 +1,50 @@
+using Messaging.Domain;
+
+namespace Messaging.Application.Services
+{
+    public class MessageService
+    {
+        private readonly MessageRepository _messageRepository;
+
+        public MessageService(MessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
+
+        public async Task<Message> CreateMessage(Sender from, Recipient to, string subject, string body)
+        {
+            var id = Guid.NewGuid();
+            var message = new Message(id, from, to, subject, body);
+            await _messageRepository.Create(message);
+            return message;
+        }
+
+        public async Task<Message> UpdateMessage(Guid id, Sender from, Recipient to, string subject, string body)
+        {
+            var message = await _messageRepository.GetById(id);
+            if (message == null)
+            {
+                throw new ArgumentException("Message not found", nameof(id));
+            }
+
+            message = new Message(id, from, to, subject, body);
+            await _messageRepository.Update(message);
+            return message;
+        }
+
+        public async Task DeleteMessage(Guid id)
+        {
+            await _messageRepository.Delete(id);
+        }
+
+        public async Task<Message> GetMessageByd(Guid id)
+        {
+            return await _messageRepository.GetById(id);
+        }
+
+        public async Task<IEnumerable<Message>> GetAllMessages()
+        {
+            return await _messageRepository.GetAll();
+        }
+    }
+}
